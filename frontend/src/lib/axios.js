d1290416e9174d4ecb85b072a603f60937b1ee11
“Daniel <danielsombo55@gmail.com>
2025-03-10 15:34:38 +0000

    Phase de développement terminée

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8001/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8001/api" : "/api",
     withCredentials: true,
 });
-// export const axiosInstance = axios.create({
-//     baseURL: import.meta.env.MODE === "development" ? "http://localhost:8001/api" : "/api",
-//     withCredentials: true,
-// });
