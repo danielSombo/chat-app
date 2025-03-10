84bcb8015173b9766c527171591650dd3bd6b48b
“Daniel <danielsombo55@gmail.com>
2025-03-10 17:11:56 +0000

    Projet Complet et terminé

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,49 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   daisyui: { 
+//     themes: ["light",
+//       "dark",
+// ]
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('@vitejs/plugin-react')) {
+              return 'vendor-framework' // Séparer React et Vite
+            }
+            if (id.includes('tailwindcss') || id.includes('@tailwindcss/vite')) {
+              return 'vendor-tailwind' // Séparer Tailwind CSS
+            }
+            return 'vendor' // Autres dépendances
+          }
+        }
+      }
+    }
+  },
   daisyui: { 
-    themes: ["light",
-      "dark",
-]
+    themes: ["light", "dark"]
   }
 })
